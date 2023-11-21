37c9fb2c4c44be2d94c5dafa0bcad89bc5d1592f
Umer Zia <umer.zia@tum.de>
2023-11-21 23:07:12 +0100

    Initial commit

---
+++ django_store_assessment/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from django_store_assessment.users.models import User
+from django_store_assessment.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
